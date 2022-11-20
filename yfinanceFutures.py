c2d9f6faaaf47caabe6e5fdf63d5e392b9d27f31
Adam Getbags <91522359+AdamGetbags@users.noreply.github.com>
2022-11-19 19:31:37 -0800

    Add files via upload

---
+++ yfinanceFutures.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: AdamGetbags
+
+"""
+
+# import modules
+import yahoo_fin.stock_info as si
+import pandas as pd
+
+# general futures ticker // could be continuous?
+ticker = "HG=F"
+                   
+# specific contract ticker                   
+# ticker = "ESZ23.CME"
+
+# request data 
+data = si.get_data(ticker, start_date = "01/01/1900",
+                   end_date = "01/01/2023") 
+
+# access the data
+closingPrices = data.adjclose
+
+# quick plot
+data.adjclose.plot()
+
